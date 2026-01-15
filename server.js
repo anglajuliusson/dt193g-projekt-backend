88aef2727a9b1b7a8931f833d6f243bc244c124f
Ängla Juliusson <angla02@icloud.com>
2026-01-15 20:03:51 +0100

    Backend kan nu leverera statiska bilder från images-mappen

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors' // Importera cors
 import { routes } from './routes/productsRoutes.js'; // Importerar route-funktionerna från productsRoutes.js
+import fastifyStatic from "@fastify/static";
+import path from "path";
 
 const fastify = Fastify({ logger: true }); 
 
@@ -9,6 +11,12 @@ const fastify = Fastify({ logger: true });
 await fastify.register(cors, {
   origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'] // Tillåt alla origins (utveckling) och alla metoder
 })
+
+// Serve statiska filer
+await fastify.register(fastifyStatic, {
+    root: path.join(process.cwd(), "images"), // "images"-mappen i projektroten
+    prefix: "/images/",                       // URL-prefix
+  });
 
 // Registrerar alla routes från routes-filen på Fastify-servern
 // Alla endpoints definierade i productsRoutes.js blir nu tillgängliga
